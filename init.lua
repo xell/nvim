a625f47af28077b361da54be1a822037305ff11c
xell <xell.liu@gmail.com>
2024-02-21 17:02:27 +0800

    Add chatgpt.nvim plugin.

--- init.lua
+++ init.lua
@@ -941,6 +941,31 @@ require("lazy").setup({
             require("ibl").setup()
         end,
     }, -- }}}
+    -- https://github.com/jackMort/ChatGPT.nvim
+    { "jackMort/ChatGPT.nvim",
+        event = "VeryLazy", -- {{{
+        config = function()
+            require("chatgpt").setup({
+                api_key_cmd = "op read op://private/OpenAI/credential --no-newline",
+                openai_params = {
+                    model = "gpt-4-turbo-preview",
+                    -- model = "gpt-4",
+                    max_tokens = 3000,
+                },
+                openai_edit_params = {
+                    model = "gpt-4-turbo-preview",
+                    -- model = "gpt-4",
+                    max_tokens = 3000,
+                },
+            })
+        end,
+        dependencies = {
+            "MunifTanjim/nui.nvim",
+            "nvim-lua/plenary.nvim",
+            "folke/trouble.nvim",
+            "nvim-telescope/telescope.nvim"
+        },
+    }, -- }}}
 }, {
     dev = {
         path = "~/Developer/vim/scripts",
